3dec141ff53d0b948ecb935466823ac0c107ce22
mikesmall <michael.l.a.small@gmail.com>
2017-08-09 23:03:53 -0400

    Step 3 complete. Works.

--- ajax.js
+++ ajax.js
@@ -1,15 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var firstButton = document.getElementById('step12button');
+  var firstButton  = document.getElementById('step12button');
+  var secondButton = document.getElementById('step3456button');
 
-  firstButton.addEventListener("click", function() {
-    console.log('The button was clicked!');
+  firstButton.addEventListener('click', function() {
+    console.log('The step 1/2 button was clicked!');
     $.ajax({
-      url: "http://first-ajax-api.herokuapp.com/",
+      url: 'http://first-ajax-api.herokuapp.com/',
       method: 'GET',
       data: '',
       dataType: 'text',
     });
   });
+
+  secondButton.addEventListener('click', function() {
+    console.log('The step 3/4/5/6 button got clicked!');
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/ping/',
+      method: 'GET',
+      data: '',
+      dataType: 'text',
+    }).done(function (responseData) {
+      console.log('Response data: ' + responseData);
+      $('#step3456').append('The server says: ' + responseData);
+    }); //'done' function
+  }); //click handler
 
 }); //DOMContentLoaded
