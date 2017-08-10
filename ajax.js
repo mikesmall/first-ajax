bd51dc6fabdf7af305a2718f6ded4cc80c646d54
mikesmall <michael.l.a.small@gmail.com>
2017-08-09 22:21:12 -0400

    Step 1 done. I think. It's hard to really know.

--- ajax.js
+++ ajax.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  /* Your code goes here */
+  // Step 1:
+  // In the linked ajax.js file, build an AJAX request that:
+  // retrieves the information at the root path of the server, by sending an empty JavaScript object, and expecting a text response.
+
+  $.ajax({
+    url: "http://first-ajax-api.herokuapp.com/",
+    method: GET,
+    data: "",
+    dataType: text
+  });
 
 });
