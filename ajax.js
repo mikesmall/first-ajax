45c638888cef5fb7b27e668c8ea693180fa1fca3
mikesmall <michael.l.a.small@gmail.com>
2017-08-09 22:43:21 -0400

    Step 2 complete. Definitely works.

--- ajax.js
+++ ajax.js
@@ -4,11 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
   // In the linked ajax.js file, build an AJAX request that:
   // retrieves the information at the root path of the server, by sending an empty JavaScript object, and expecting a text response.
 
-  $.ajax({
-    url: "http://first-ajax-api.herokuapp.com/",
-    method: GET,
-    data: "",
-    dataType: text
-  });
+  // Back in ajax.js, Create a 'click' event handler for the button, and move your AJAX call inside it.
 
-});
+  var firstButton = document.getElementById('step12button');
+
+  firstButton.addEventListener("click", function() {
+
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/",
+      method: 'GET',
+      data: '',
+      dataType: 'text',
+    });
+
+    console.log('The button was clicked!');
+
+  }); // firstButton.addEventListener
+
+}); // DOMContentLoaded
