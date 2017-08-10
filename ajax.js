f8ba1013b71cd407a79f5bc392af1b2507574813
mikesmall <michael.l.a.small@gmail.com>
2017-08-09 22:48:57 -0400

    Refactored to remove comments, place console.log above ajax call

--- ajax.js
+++ ajax.js
@@ -1,24 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Step 1:
-  // In the linked ajax.js file, build an AJAX request that:
-  // retrieves the information at the root path of the server, by sending an empty JavaScript object, and expecting a text response.
-
-  // Back in ajax.js, Create a 'click' event handler for the button, and move your AJAX call inside it.
-
   var firstButton = document.getElementById('step12button');
 
   firstButton.addEventListener("click", function() {
-
+    console.log('The button was clicked!');
     $.ajax({
       url: "http://first-ajax-api.herokuapp.com/",
       method: 'GET',
       data: '',
       dataType: 'text',
     });
+  });
 
-    console.log('The button was clicked!');
-
-  }); // firstButton.addEventListener
-
-}); // DOMContentLoaded
+}); //DOMContentLoaded
